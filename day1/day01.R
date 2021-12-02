27a1eb04cee66bbf4d571da5bbcf2cd7bad5a1f6
Kevin in HomeWSL <kevin820606@gmail.com>
2021-12-03 03:07:44 +0800

    add R version day1

---
+++ day1/day01.R
@@ -0,0 +1,18 @@
+#! /usr/bin/R
+
+# read file
+data_1 <- read.table("data/data_01.txt")
+# Lag 1 data point
+data_1$V1 |>
+    diff() |>
+    {
+        function(x) x > 0
+    }() |>
+    sum()
+# Lag 3 data point
+data_1$V1 |>
+    diff(3) |>
+    {
+        function(x) x > 0
+    }() |>
+    sum()
