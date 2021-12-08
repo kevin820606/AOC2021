c503c73dfa146f9c2062a24feff174ad7ae35ad7
Kevin_laptop <kevin820606@gmail.com>
2021-12-08 18:53:22 +0800

    not finish

--- day6/day06.R
+++ day6/day06.R
@@ -1,1 +1,11 @@
-# there're probably not different solution for Counter Version of python code.
+#!/usr/bin/Rscript
+
+number_vec <- readLines("data/data_06_example.txt") |>
+    strsplit(",") |>
+    unlist() |>
+    as.numeric()
+numbertable <- table(number_vec)
+for (i in 1:18) {
+    zero <- number_table[0]
+    number_table[0]
+}
