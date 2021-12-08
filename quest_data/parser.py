b5ba2619e52c58caa6d2ad0815cd79a1348c5678
Kevin_laptop <kevin820606@gmail.com>
2021-12-08 21:51:16 +0800

    try to parse

--- quest_data/parser.py
+++ quest_data/parser.py
@@ -1,6 +1,13 @@
 #! /usr/bin/env python3
+from pathlib import Path
+
+
 def read(day: int, example: bool = False) -> list[str]:
-    filename = f"data_{day:2d}" + "_example" * example
+    filename = f"{Path(__file__).parent}/data_{day:02d}" + "_example" * example
     with open(file=f"{filename}.txt", mode="r") as raw:
         data = raw.read().splitlines()
     return data
+
+
+if __name__ == "__main__":
+    print(read(7, True))
