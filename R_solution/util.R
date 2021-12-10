59d6e355947abcca343914796b21985be65a784d
Kevin_laptop <kevin820606@gmail.com>
2021-12-10 18:52:16 +0800

    R help func

---
+++ R_solution/util.R
@@ -0,0 +1,8 @@
+#! /usr/bin/Rscript
+read_question <- function(day, example) {
+    origin <- sprintf("../data/data_%02d", day)
+    path <- ifelse(example,
+        sprintf("%s_example.txt", origin), sprintf("%s.txt", origin)
+    )
+    read.table(path, col.names = "Question_input")
+}
