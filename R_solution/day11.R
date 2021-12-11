450bbceeb5981d18576f3fefb618a06825bd6f50
Kevin in HomeWSL <kevin820606@gmail.com>
2021-12-12 02:32:42 +0800

    10 times faster than python

--- R_solution/day11.R
+++ R_solution/day11.R
@@ -1,6 +1,6 @@
 #! /usr/bin/Rscript
 source("util.R")
-raw_data <- read_question(11, T)
+raw_data <- read_question(11, F)
 split_to_matrix <- function(lines) {
     mat <- matrix(ncol = 10, nrow = 0)
 
@@ -11,29 +11,49 @@ split_to_matrix <- function(lines) {
     mat
 }
 
-get_surround <- function(point, dont_move) {
-    move <- c(-11, -10, -9, -1, 1, 9, 10, 11)
-    aftermove <- point + move
-    aftermove[aftermove > 0 & aftermove < 101 & !aftermove %in% dont_move]
+get_surround <- function(point) {
+    back_move <- c(-11, -1, 9) + point
+    horizon_move <- c(-10, 10) + point
+    forward_move <- c(-9, 1, 11) + point
+    all_point <- c(
+        back_move[back_move %% 10 != 0],
+        horizon_move, forward_move[(forward_move - 1) %% 10 != 0]
+    )
+    all_point[all_point > 0 & all_point < 101]
 }
-
 add_energy <- function(mat) {
     flash <- 1:100
     flashed <- c()
-    while (length(flash)) {
+    repeat {
         for (fl in flash) mat[fl] <- mat[fl] + 1
-        flash <- sapply(which(mat >= 10), get_surround, flashed)
-        print(flash)
+        poppoint <- which(mat >= 10)[!which(mat >= 10) %in% flashed]
+        flash <- sapply(poppoint, get_surround)
         flashed <- unique(c(flashed, which(mat >= 10)))
+        if (sum(unlist(flash)) == 0) break
     }
     mat[flashed] <- 0
     mat
 }
 
 q1_solution <- function(mat, times) {
-    for (i in 1:times) mat <- add_energy(mat)
-    mat
+    init <- 0
+    for (i in 1:times) {
+        mat <- add_energy(mat)
+        init <- init + sum(mat == 0)
+    }
+    init
+}
+
+q2_solution <- function(mat) {
+    times <- 0
+    repeat{
+        times <- times + 1
+        mat <- add_energy(mat)
+        if (length(unique(as.vector(mat))) == 1) break
+    }
+    times
 }
 
 question_mat <- split_to_matrix(raw_data$Question_input)
-print(q1_solution(question_mat, 2))
+print(q1_solution(question_mat, 100))
+print(q2_solution(question_mat))
